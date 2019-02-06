075a21130ea675ba001330b25a2a488e2278aae2
Eagan Martin <mece.martinece@gmail.com>
2019-02-06 17:02:08 +0800

    Added add arithmetic endpoint

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 #!compute-api/bin/python
 from flask import Flask, jsonify, make_response, request
+from arithmetic import Arithmetic
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return make_response(jsonify{
+	return make_response(jsonify({
 			'status': 'success',
 			'message': 'Welcome to compute API'
-		}, 200)
+		}), 200)
 
 @app.errorhandler(404)
 def not_found(error):
@@ -24,5 +25,14 @@ def not_found(error):
     		'message': 'Bad request' 
     	} ), 400)
 
+class Arithmetic():
+	@app.route('/compute/arithmetic/add', methods = ['POST'])
+	def add():
+		if not request.json: abort(400)
+		return jsonify({
+			'status': 'success',
+			'result': request.json['a'] + request.json['b']
+		}), 200
+
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug = True)
