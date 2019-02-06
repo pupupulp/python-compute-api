a875bff16beec381fffcd41c8956fc71e6cc7507
Eagan Martin <mece.martinece@gmail.com>
2019-02-06 16:34:02 +0800

    Revised json return of error handlers

--- app.py
+++ app.py
@@ -5,15 +5,24 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return "Test"
+	return make_response(jsonify{
+			'status': 'success',
+			'message': 'Welcome to compute API'
+		}, 200)
 
 @app.errorhandler(404)
 def not_found(error):
-	return make_response(jsonify({ 'error': 'Not found' }), 404)
+	return make_response(jsonify({ 
+			'status': 'error',
+			'message': 'Not found' 
+		}), 404)
 
 @app.errorhandler(400)
 def not_found(error):
-    return make_response(jsonify( { 'error': 'Bad request' } ), 400)
+    return make_response(jsonify({ 
+    		'status': 'error',
+    		'message': 'Bad request' 
+    	} ), 400)
 
 if __name__ == '__main__':
 	app.run(debug=True)
